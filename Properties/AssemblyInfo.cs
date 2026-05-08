b46f6f71ba1e0b0c601e11bff6aba0652f9b802e
Carl Björk <carl.bjork@gmail.com>
2026-05-08 23:45:29 +0200

    Increment version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 //Should be incremented for each new release build of a plugin
 
 // Odd minor releases for Beta
-[assembly: AssemblyVersion("3.29.0.10")]
-[assembly: AssemblyFileVersion("3.29.0.10")]
+[assembly: AssemblyVersion("3.29.0.11")]
+[assembly: AssemblyFileVersion("3.29.0.11")]
 
 // [MANDATORY] The name of your plugingit st
 [assembly: AssemblyTitle("Sequencer+")]
